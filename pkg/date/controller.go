1cf5c9dd32b8a31fd96b9d2c43da59c417954de3
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2026-01-30 09:45:01 +0100

    add controller and models for the date part

--- pkg/date/controller.go
+++ pkg/date/controller.go
@@ -1,1 +1,254 @@
 package date
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"yplanning/config"
+	"yplanning/database/dbmodel"
+	"yplanning/pkg/models"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+)
+
+type DateConfig struct {
+	*config.Config
+}
+
+func NewDateConfig(cfg *config.Config) *DateConfig {
+	return &DateConfig{Config: cfg}
+}
+
+func (config *DateConfig) GetAllDates(w http.ResponseWriter, r *http.Request) {
+	dates, err := config.DateRepository.FindAll()
+	if err != nil {
+		fmt.Println("Failed to retrieve dates")
+		return
+	}
+	DateResponse := make([]models.DateResponse, 0)
+	for _, date := range dates {
+		DateResponse = append(DateResponse, models.DateResponse{
+			ID:           date.ID,
+			Title:        date.Title,
+			Body:         date.Body,
+			DateBegin:    date.BeginTime,
+			DateEnd:      date.EndTime,
+			UserID:       date.UserID,
+			Private:      date.Private,
+			RecurrenceID: date.RecurrenceID,
+			ColorID:      date.ColorID,
+		})
+	}
+	render.JSON(w, r, DateResponse)
+}
+
+func (config *DateConfig) GetDateByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		fmt.Println("Error during id convertion")
+	}
+	if id < 1 {
+		render.JSON(w, r, map[string]string{"error": "id must be >= 1"})
+		return
+	}
+	date, err := config.DateRepository.FindByID(uint(id))
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to retrieve date"})
+		return
+	}
+	dateResponse := &models.DateResponse{
+		ID:           date.ID,
+		Title:        date.Title,
+		Body:         date.Body,
+		DateBegin:    date.BeginTime,
+		DateEnd:      date.EndTime,
+		UserID:       date.UserID,
+		Private:      date.Private,
+		RecurrenceID: date.RecurrenceID,
+		ColorID:      date.ColorID,
+	}
+	render.JSON(w, r, dateResponse)
+}
+
+func (config *DateConfig) GetDatesByUserID(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.Atoi(chi.URLParam(r, "user_id"))
+	if err != nil {
+		fmt.Println("Error during user_id convertion")
+	}
+	if userID < 1 {
+		render.JSON(w, r, map[string]string{"error": "user_id must be >= 1"})
+		return
+	}
+	date, err := config.DateRepository.FindByUserID(uint(userID))
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to retrieve date"})
+		return
+	}
+	dateResponse := &models.DateResponse{
+		ID:           date.ID,
+		Title:        date.Title,
+		Body:         date.Body,
+		DateBegin:    date.BeginTime,
+		DateEnd:      date.EndTime,
+		UserID:       date.UserID,
+		Private:      date.Private,
+		RecurrenceID: date.RecurrenceID,
+		ColorID:      date.ColorID,
+	}
+	render.JSON(w, r, dateResponse)
+}
+
+func (config *DateConfig) GetDatesByRecurrenceID(w http.ResponseWriter, r *http.Request) {
+	recurrenceID, err := strconv.Atoi(chi.URLParam(r, "recurrence_id"))
+	if err != nil {
+		fmt.Println("Error during recurrence_id convertion")
+	}
+	if recurrenceID < 1 {
+		render.JSON(w, r, map[string]string{"error": "recurrence_id must be >= 1"})
+		return
+	}
+	date, err := config.DateRepository.FindByRecurrenceID(uint(recurrenceID))
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to retrieve date"})
+		return
+	}
+	dateResponse := &models.DateResponse{
+		ID:           date.ID,
+		Title:        date.Title,
+		Body:         date.Body,
+		DateBegin:    date.BeginTime,
+		DateEnd:      date.EndTime,
+		UserID:       date.UserID,
+		Private:      date.Private,
+		RecurrenceID: date.RecurrenceID,
+		ColorID:      date.ColorID,
+	}
+	render.JSON(w, r, dateResponse)
+}
+
+func (config *DateConfig) GetDateByDayRange(w http.ResponseWriter, r *http.Request) {
+	var rangeRequest models.RangeRequest
+
+	if rangeRequest.DateBegin.IsZero() {
+		render.JSON(w, r, map[string]string{"error": "date_begin must not be null"})
+		return
+	} else if rangeRequest.DateEnd.IsZero() {
+		render.JSON(w, r, map[string]string{"error": "date_end must not be null"})
+		return
+	} else if rangeRequest.UserID < 1 {
+		render.JSON(w, r, map[string]string{"error": "user_id must be >= 1"})
+		return
+	}
+	date := &models.RangeRequest{
+		DateBegin: rangeRequest.DateBegin,
+		DateEnd:   rangeRequest.DateEnd,
+		UserID:    rangeRequest.UserID,
+	}
+
+	dates, err := config.DateRepository.FindByDayRange(date.DateBegin, date.DateEnd, uint(date.UserID))
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to retrieve dates"})
+		return
+	}
+	DateResponse := make([]models.DateResponse, 0)
+	for _, date := range dates {
+		DateResponse = append(DateResponse, models.DateResponse{
+			ID:           date.ID,
+			Title:        date.Title,
+			Body:         date.Body,
+			DateBegin:    date.BeginTime,
+			DateEnd:      date.EndTime,
+			UserID:       date.UserID,
+			Private:      date.Private,
+			RecurrenceID: date.RecurrenceID,
+			ColorID:      date.ColorID,
+		})
+	}
+	render.JSON(w, r, DateResponse)
+}
+
+func (config *DateConfig) CreateDate(w http.ResponseWriter, r *http.Request) {
+	var dateRequest models.DateRequest
+	if err := render.Bind(r, &dateRequest); err != nil {
+		render.JSON(w, r, map[string]string{"error": "Invalid request payload"})
+		return
+	}
+	date := &dbmodel.Date{
+		Title:        dateRequest.Title,
+		Body:         dateRequest.Body,
+		BeginTime:    dateRequest.DateBegin,
+		EndTime:      dateRequest.DateEnd,
+		UserID:       dateRequest.UserID,
+		Private:      dateRequest.Private,
+		RecurrenceID: dateRequest.RecurrenceID,
+		ColorID:      dateRequest.ColorID,
+	}
+	createdDate, err := config.DateRepository.Create(date)
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to create date"})
+		return
+	}
+	dateResponse := &models.DateResponse{
+		ID:           createdDate.ID,
+		Title:        createdDate.Title,
+		Body:         createdDate.Body,
+		DateBegin:    createdDate.BeginTime,
+		DateEnd:      createdDate.EndTime,
+		UserID:       createdDate.UserID,
+		Private:      createdDate.Private,
+		RecurrenceID: createdDate.RecurrenceID,
+		ColorID:      createdDate.ColorID,
+	}
+	render.JSON(w, r, dateResponse)
+}
+
+func (config *DateConfig) UpdateDate(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		fmt.Println("Error during id convertion")
+	}
+	if id < 1 {
+		render.JSON(w, r, map[string]string{"error": "id must be >= 1"})
+		return
+	}
+	var dateRequest models.DateRequest
+	if err := render.Bind(r, &dateRequest); err != nil {
+		render.JSON(w, r, map[string]string{"error": "Invalid request payload"})
+		return
+	}
+	date := &dbmodel.Date{
+		Title:        dateRequest.Title,
+		Body:         dateRequest.Body,
+		BeginTime:    dateRequest.DateBegin,
+		EndTime:      dateRequest.DateEnd,
+		UserID:       dateRequest.UserID,
+		Private:      dateRequest.Private,
+		RecurrenceID: dateRequest.RecurrenceID,
+		ColorID:      dateRequest.ColorID,
+	}
+	err = config.DateRepository.UpdateByID(uint(id), date)
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to update date"})
+		return
+	}
+	render.JSON(w, r, map[string]string{"message": "Date updated successfully"})
+}
+
+func (config *DateConfig) DeleteDate(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		fmt.Println("Error during id convertion")
+	}
+	if id < 1 {
+		render.JSON(w, r, map[string]string{"error": "id must be >= 1"})
+		return
+	}
+	err = config.DateRepository.DeleteByID(uint(id))
+	if err != nil {
+		render.JSON(w, r, map[string]string{"error": "Failed to delete date"})
+		return
+	}
+	render.JSON(w, r, map[string]string{"message": "Date deleted successfully"})
+}
--- pkg/models/date.go
+++ pkg/models/date.go
@@ -1,1 +1,49 @@
 package models
+
+import (
+	"errors"
+	"net/http"
+	"time"
+)
+
+type DateRequest struct {
+	Title        string    `json:"title"`
+	Body         string    `json:"body"`
+	DateBegin    time.Time `json:"date_begin"`
+	DateEnd      time.Time `json:"date_end"`
+	UserID       uint      `json:"user_id"`
+	Private      bool      `json:"private"`
+	RecurrenceID uint      `json:"recurrence_id"`
+	ColorID      uint      `json:"color_id"`
+}
+
+type RangeRequest struct {
+	DateBegin time.Time `json:"date_begin"`
+	DateEnd   time.Time `json:"date_end"`
+	UserID    uint      `json:"user_id"`
+}
+
+func (u *DateRequest) Bind(r *http.Request) error {
+	if u.Title == "" {
+		return errors.New("title must not be null")
+	} else if u.DateBegin.IsZero() {
+		return errors.New("date_begin must not be null")
+	} else if u.DateEnd.IsZero() {
+		return errors.New("date_end must not be null")
+	} else if u.UserID < 1 {
+		return errors.New("user_id must be >= 1")
+	}
+	return nil
+}
+
+type DateResponse struct {
+	ID           uint      `json:"id"`
+	Title        string    `json:"title"`
+	Body         string    `json:"body"`
+	DateBegin    time.Time `json:"date_begin"`
+	DateEnd      time.Time `json:"date_end"`
+	UserID       uint      `json:"user_id"`
+	Private      bool      `json:"private"`
+	RecurrenceID uint      `json:"recurrence_id"`
+	ColorID      uint      `json:"color_id"`
+}
