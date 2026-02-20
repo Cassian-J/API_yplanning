1805eea38723f2289801210a9febf96a389cb4f2
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2026-02-20 10:24:57 +0100

    add the main.go part

--- go.mod
+++ go.mod
@@ -7,21 +7,35 @@ require (
 	github.com/go-chi/chi/v5 v5.2.4
 	github.com/go-chi/render v1.0.3
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/joho/godotenv v1.5.1
+	github.com/swaggo/http-swagger v1.3.4
 	golang.org/x/crypto v0.48.0
 	gorm.io/gorm v1.31.1
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/glebarez/go-sqlite v1.21.2 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/tools v0.41.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	modernc.org/libc v1.22.5 // indirect
 	modernc.org/mathutil v1.5.0 // indirect
 	modernc.org/memory v1.5.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"yplanning/config"
+	"yplanning/pkg/authentication"
+	"yplanning/pkg/availability"
+	"yplanning/pkg/color"
+	"yplanning/pkg/date"
+	"yplanning/pkg/group"
+	"yplanning/pkg/user"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+// @title			LocateThis API
+// @version			1.0
+// @host			localhost:8080
+// @BasePath		/api
+// @securityDefinitions.apikey	BearerAuth
+// @in				header
+// @name			Authorization
+func Routes(configuration *config.Config) *chi.Mux {
+	router := chi.NewRouter()
+	router.Get("/swagger/*", httpSwagger.WrapHandler)
+
+	router.Mount("/api/auth", authentication.Routes(configuration))
+
+	router.Group(func(r chi.Router) {
+		r.Use(authentication.AuthMiddleware(os.Getenv("JWT_SECRET")))
+		r.Mount("/api/groups", group.Routes(configuration))
+		r.Mount("/api/date", date.Routes(configuration))
+		r.Mount("/api/availability", availability.Routes(configuration))
+		r.Mount("/api/color", color.Routes(configuration))
+		r.Mount("/api/users", user.Routes(configuration))
+	})
+
+	return router
+}
+
+func main() {
+	// Initialisation de la configuration
+	configuration, err := config.New()
+	if err != nil {
+		log.Panicln("Configuration error:", err)
+	}
+	godotenv.Load()
+	// Initialisation des routes
+	router := Routes(configuration)
+
+	log.Println("Server running on http://localhost:" + os.Getenv("PORT"))
+	log.Println("Swagger UI available at http://localhost:" + os.Getenv("PORT") + "/swagger/index.html")
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
